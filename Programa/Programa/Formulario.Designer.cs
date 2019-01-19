2374ae6744325f58512c6f5ddcabac87e09bf1cf
vpinob <46831704+vpinob@users.noreply.github.com>
2019-01-19 21:15:55 +0100

    Add files via upload

---
+++ Programa/Programa/Formulario.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Programa
+{
+    partial class Formulario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.BotonGuardar = new System.Windows.Forms.Button();
+            this.BotonSalir = new System.Windows.Forms.Button();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BotonCargar = new System.Windows.Forms.Button();
+            this.RutaArchivo = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.GroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.BotonGuardar);
+            this.GroupBox1.Controls.Add(this.BotonSalir);
+            this.GroupBox1.Controls.Add(this.DataGridView1);
+            this.GroupBox1.Controls.Add(this.BotonCargar);
+            this.GroupBox1.Controls.Add(this.RutaArchivo);
+            this.GroupBox1.Location = new System.Drawing.Point(8, 4);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(768, 352);
+            this.GroupBox1.TabIndex = 0;
+            this.GroupBox1.TabStop = false;
+            // 
+            // BotonGuardar
+            // 
+            this.BotonGuardar.Location = new System.Drawing.Point(544, 320);
+            this.BotonGuardar.Name = "BotonGuardar";
+            this.BotonGuardar.Size = new System.Drawing.Size(104, 24);
+            this.BotonGuardar.TabIndex = 2;
+            this.BotonGuardar.Text = "Save";
+            this.BotonGuardar.UseVisualStyleBackColor = true;
+            this.BotonGuardar.Click += new System.EventHandler(this.BotonGuardar_Click);
+            // 
+            // BotonSalir
+            // 
+            this.BotonSalir.Location = new System.Drawing.Point(656, 320);
+            this.BotonSalir.Name = "BotonSalir";
+            this.BotonSalir.Size = new System.Drawing.Size(104, 24);
+            this.BotonSalir.TabIndex = 3;
+            this.BotonSalir.Text = "Exit";
+            this.BotonSalir.UseVisualStyleBackColor = true;
+            this.BotonSalir.Click += new System.EventHandler(this.BotonSalir_Click);
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.ColumnHeadersHeight = 24;
+            this.DataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.DataGridView1.Location = new System.Drawing.Point(8, 48);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.ReadOnly = true;
+            this.DataGridView1.RowHeadersVisible = false;
+            this.DataGridView1.RowTemplate.ReadOnly = true;
+            this.DataGridView1.Size = new System.Drawing.Size(752, 264);
+            this.DataGridView1.TabIndex = 4;
+            this.DataGridView1.TabStop = false;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Register";
+            this.Column1.MaxInputLength = 12;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 120;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Invoice";
+            this.Column2.MaxInputLength = 12;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 120;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Nin";
+            this.Column3.MaxInputLength = 12;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 120;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Digit";
+            this.Column4.MaxInputLength = 12;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 120;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Id";
+            this.Column5.MaxInputLength = 12;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 120;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Amount";
+            this.Column6.MaxInputLength = 12;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 120;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Date";
+            this.Column7.MaxInputLength = 12;
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 120;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "State";
+            this.Column8.MaxInputLength = 12;
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 120;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Banc";
+            this.Column9.MaxInputLength = 12;
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 120;
+            // 
+            // BotonCargar
+            // 
+            this.BotonCargar.Location = new System.Drawing.Point(656, 16);
+            this.BotonCargar.Name = "BotonCargar";
+            this.BotonCargar.Size = new System.Drawing.Size(104, 24);
+            this.BotonCargar.TabIndex = 1;
+            this.BotonCargar.Text = "Load";
+            this.BotonCargar.UseVisualStyleBackColor = true;
+            this.BotonCargar.Click += new System.EventHandler(this.BotonCargar_Click);
+            // 
+            // RutaArchivo
+            // 
+            this.RutaArchivo.BackColor = System.Drawing.Color.White;
+            this.RutaArchivo.Font = new System.Drawing.Font("Gisha", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RutaArchivo.Location = new System.Drawing.Point(8, 16);
+            this.RutaArchivo.Multiline = true;
+            this.RutaArchivo.Name = "RutaArchivo";
+            this.RutaArchivo.ReadOnly = true;
+            this.RutaArchivo.Size = new System.Drawing.Size(640, 24);
+            this.RutaArchivo.TabIndex = 0;
+            this.RutaArchivo.TabStop = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "txt";
+            this.openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.openFileDialog.InitialDirectory = "C:\\";
+            this.openFileDialog.Multiselect = true;
+            this.openFileDialog.RestoreDirectory = true;
+            this.openFileDialog.Title = "Open";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "xls";
+            this.saveFileDialog.FileName = "File";
+            this.saveFileDialog.Filter = "Excel (*.xls)|*.xls|All files(*.*) | *.* ";
+            this.saveFileDialog.InitialDirectory = "C:\\";
+            this.saveFileDialog.Title = "Save as";
+            // 
+            // Formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.GroupBox1);
+            this.Name = "Formulario";
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox GroupBox1;
+        private System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.Button BotonCargar;
+        private System.Windows.Forms.TextBox RutaArchivo;
+        private System.Windows.Forms.Button BotonGuardar;
+        private System.Windows.Forms.Button BotonSalir;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Programa/Programa/Formulario.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace Programa
+{
+    public partial class Formulario : Form
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void BotonSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BotonCargar_Click(object sender, EventArgs e)
+        {
+            LoadFiles();
+        }
+
+        private void BotonGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DataGridView1[0, 1].Value.ToString() != String.Empty)
+                    CreateExcelFile();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show(this, "File not upload.", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void LoadFiles()
+        {
+            string[] dataArray = null;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    RutaArchivo.Text = Path.GetDirectoryName(openFileDialog.FileName);
+
+                    foreach (String fileName in openFileDialog.FileNames)
+                    {
+                        StreamReader streamReader = new StreamReader(fileName);
+                        string stringLine = "";
+
+                        while (stringLine != null)
+                        {
+                            stringLine = streamReader.ReadLine();
+
+                            if (stringLine != null)
+                            {
+                                dataArray = stringLine.Split(';');
+                                DataGridView1.Rows.Add(dataArray[0], dataArray[1], dataArray[2], dataArray[3], dataArray[4], dataArray[5], dataArray[6], dataArray[7], dataArray[8]);
+                            }  
+                        }
+                        streamReader.Close();
+                    }
+                }
+                catch (IndexOutOfRangeException) 
+                {
+                    MessageBox.Show(this, "File content not supported.", "Error", MessageBoxButtons.OK);
+                }
+                
+            } 
+        }
+
+        private void CreateExcelFile()
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Microsoft.Office.Interop.Excel.Application ExcelApplication = default(Microsoft.Office.Interop.Excel.Application);          
+                Microsoft.Office.Interop.Excel.Workbook workBook = default(Microsoft.Office.Interop.Excel.Workbook);
+                Microsoft.Office.Interop.Excel.Worksheet workSheet = default(Microsoft.Office.Interop.Excel.Worksheet);
+
+                ExcelApplication = new Microsoft.Office.Interop.Excel.Application
+                {
+                    Visible = true
+                };
+
+                workBook = ExcelApplication.Workbooks.Add();
+                workBook.Worksheets[1].Name = "Report";
+                workBook.Worksheets[2].Name = "Pivot Table";
+                workBook.Worksheets[3].Name = "To check";
+
+                workSheet = workBook.Worksheets[1];
+                workSheet.Visible = Microsoft.Office.Interop.Excel.XlSheetVisibility.xlSheetVisible;
+                workSheet.Activate();
+
+                ExportToExcel(workSheet, DataGridView1);
+
+                Microsoft.Office.Interop.Excel.Range formatRange = workSheet.get_Range("A1", "I1");
+                formatRange.Font.Bold = true;
+                formatRange.Columns.ColumnWidth = 15;
+                workSheet.UsedRange.Columns.Select();
+
+                workBook.SaveAs(saveFileDialog.FileName, XlFileFormat.xlWorkbookNormal,
+                                                System.Reflection.Missing.Value, System.Reflection.Missing.Value, false, false,
+                                                XlSaveAsAccessMode.xlShared, false, false,
+                                                System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value);
+            }
+        }
+
+        private void ExportToExcel(Microsoft.Office.Interop.Excel.Worksheet workSheet, DataGridView dataGridView)
+        {
+            int iCol = 0;
+
+            foreach (DataGridViewColumn column in dataGridView.Columns)
+                if (column.Visible)
+                    workSheet.Cells[1, ++iCol] = column.HeaderText;
+
+            for (int i = 0; i < dataGridView.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView.Columns.Count; j++)
+                {
+                    workSheet.Cells[i + 2, j + 1] = dataGridView.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+
+        }
+
+        private void CreatePivotTable(Microsoft.Office.Interop.Excel.Worksheet workSheet)
+        {
+
+        }
+    }
+}
